bc81c7049a927f275b56f8f142347e65b6e6dbe7
Dan Zhuravlov <yuoirty@gmail.com>
2023-05-04 16:07:06 +0300

    Added table classes

---
+++ app/src/main/java/com/example/sqlandcrying/Grades.java
@@ -0,0 +1,9 @@
+package com.example.sqlandcrying;
+
+public class Grades {
+    public static final String TABLE_GRADES = "Grades";
+    public static final String QUARTER = "quarter";
+    public static final String subject = "Subject";
+    public static final String KEY_PHONE_NUMBER = "phone";
+    public static final String GRADE = "grade";
+}
---
+++ app/src/main/java/com/example/sqlandcrying/Students.java
@@ -0,0 +1,15 @@
+package com.example.sqlandcrying;
+
+public class Students {
+    public static final String TABLE_STUDENTS = "Students";
+    public static final String NAME = "name";
+    public static final String IS_ACTIVE = "active";
+    public static final String ADDRESS = "Address";
+    public static final String KEY_PHONE_NUMBER = "phone";
+    public static final String HOME_PHONE = "homephone";
+    public static final String MOM_NAME = "mom";
+    public static final String MOM_PHONE = "momphone";
+    public static final String DAD_NAME = "dad";
+    public static final String DAD_PHONE = "dadphone";
+
+}
