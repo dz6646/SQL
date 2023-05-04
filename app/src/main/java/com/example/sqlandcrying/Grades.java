10db4790f30182c754cae16894e560c738ef9f53
Dan Zhuravlov <yuoirty@gmail.com>
2023-05-04 16:27:14 +0300

    Added tables

--- app/src/main/java/com/example/sqlandcrying/Grades.java
+++ app/src/main/java/com/example/sqlandcrying/Grades.java
@@ -3,7 +3,7 @@ package com.example.sqlandcrying;
 public class Grades {
     public static final String TABLE_GRADES = "Grades";
     public static final String QUARTER = "quarter";
-    public static final String subject = "Subject";
+    public static final String SUBJECT = "Subject";
     public static final String KEY_PHONE_NUMBER = "phone";
     public static final String GRADE = "grade";
 }
--- app/src/main/java/com/example/sqlandcrying/HelperDB.java
+++ app/src/main/java/com/example/sqlandcrying/HelperDB.java
@@ -13,11 +13,36 @@ public class HelperDB extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        createStudents(sqLiteDatabase);
+        createGrades(sqLiteDatabase);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+
+    public void createGrades(SQLiteDatabase db)
+    {
+        String strCreate = "CREATE TABLE IF NOT EXISTS " + Grades.TABLE_GRADES;
+        strCreate += " (" + Grades.KEY_PHONE_NUMBER + " TEXT PRIMARY KET NOT NULL,";
+        strCreate += " " + Grades.QUARTER + " INTEGER,";
+        strCreate += " " + Grades.SUBJECT + " TEXT NOT NULL,";
+        strCreate += " " + Grades.GRADE + " INTEGER);";
+
+    }
+
+    public void createStudents(SQLiteDatabase db)
+    {
+        String strCreate = "CREATE TABLE IF NOT EXISTS " + Students.TABLE_STUDENTS;
+        strCreate += " (" + Students.KEY_PHONE_NUMBER + " TEXT PRIMARY KEY NOT NULL,";
+        strCreate += " " + Students.NAME + " TEXT NOT NULL,";
+        strCreate += " " + Students.ADDRESS + " TEXT NOT NULL,";
+        strCreate += " " + Students.IS_ACTIVE + " INTEGER,";
+        strCreate += " " + Students.DAD_NAME + " TEXT NOT NULL,";
+        strCreate += " " + Students.DAD_PHONE + " TEXT NOT NULL,";
+        strCreate += " " + Students.MOM_NAME + " TEXT NOT NULL,";
+        strCreate += " " + Students.MOM_PHONE + " TEXT NOT NULL,";
+        strCreate += " " + Students.HOME_PHONE + " TEXT NOT NULL);";
     }
 }
